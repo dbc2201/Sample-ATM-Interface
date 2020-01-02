5f8e8469115bec24c1ed5dcb32584b01014fcdc6
dbc2201 <divyanshb22@gmail.com>
2020-01-02 22:13:26 +0530

    add comment for modifiers for method signatures

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -4,5 +4,6 @@ package atm;
  * This is an Interface for an ATM
  */
 public interface ATM {
-
+    // Since all the methods that are declared inside an Interface in Java are
+    // `public abstract` already (by default), we do not need to add any modifiers.
 }
